a3444f51b19125017bf02df3391702a89b54a7b5
Dr Nic Williams <drnicwilliams@gmail.com>
2012-11-05 11:18:19 -0800

    tabs to spaces

--- assets/themes/smart-business-template/js/custom.js
+++ assets/themes/smart-business-template/js/custom.js
@@ -1,94 +1,94 @@
 jQuery(document).ready(function($){
-	
-	/* ------------------- Fancybox --------------------- */
+  
+  /* ------------------- Fancybox --------------------- */
 
-	(function() {
+  (function() {
 
-		$('[rel=image]').fancybox({
-			type        : 'image',
-			openEffect  : 'fade',
-			closeEffect	: 'fade',
-			nextEffect  : 'fade',
-			prevEffect  : 'fade',
-			helpers     : {
-				title   : {
-					type : 'inside'
-				}
-			}
-		});
+    $('[rel=image]').fancybox({
+      type        : 'image',
+      openEffect  : 'fade',
+      closeEffect  : 'fade',
+      nextEffect  : 'fade',
+      prevEffect  : 'fade',
+      helpers     : {
+        title   : {
+          type : 'inside'
+        }
+      }
+    });
 
-		$('[rel=image-gallery]').fancybox({
-			nextEffect  : 'fade',
-			prevEffect  : 'fade',
-			helpers     : {
-				title   : {
-					type : 'inside'
-				},
-				buttons  : {},
-				media    : {}
-			}
-		});
+    $('[rel=image-gallery]').fancybox({
+      nextEffect  : 'fade',
+      prevEffect  : 'fade',
+      helpers     : {
+        title   : {
+          type : 'inside'
+        },
+        buttons  : {},
+        media    : {}
+      }
+    });
 
 
-	})();
-	
-	
-	/* ------------------- Client Carousel --------------------- */
+  })();
+  
+  
+  /* ------------------- Client Carousel --------------------- */
 
-	$('.clients-carousel').flexslider({
-	    animation: "slide",
-		easing: "swing",
-	    animationLoop: true,
-	    itemWidth: 1,
-	    itemMargin: 1,
-	    minItems: 1,
-	    maxItems: 8,
-		controlNav: false,
-		directionNav: false,
-		move: 2
+  $('.clients-carousel').flexslider({
+      animation: "slide",
+    easing: "swing",
+      animationLoop: true,
+      itemWidth: 1,
+      itemMargin: 1,
+      minItems: 1,
+      maxItems: 8,
+    controlNav: false,
+    directionNav: false,
+    move: 2
       });
 
 
-	/* ------------------ Back To Top ------------------- */
+  /* ------------------ Back To Top ------------------- */
 
-	jQuery('#footer-menu-back-to-top a').click(function(){
-		jQuery('html, body').animate({scrollTop:0}, 300); 
-		return false; 
-	});
-	
+  jQuery('#footer-menu-back-to-top a').click(function(){
+    jQuery('html, body').animate({scrollTop:0}, 300); 
+    return false; 
+  });
+  
 
-	/* --------------------- Tabs ------------------------ */	
+  /* --------------------- Tabs ------------------------ */  
 
-		(function() {
+    (function() {
 
-			var $tabsNav    = $('.tabs-nav'),
-				$tabsNavLis = $tabsNav.children('li'),
-				$tabContent = $('.tab-content');
+      var $tabsNav    = $('.tabs-nav'),
+        $tabsNavLis = $tabsNav.children('li'),
+        $tabContent = $('.tab-content');
 
-			$tabsNav.each(function() {
-				var $this = $(this);
+      $tabsNav.each(function() {
+        var $this = $(this);
 
-				$this.next().children('.tab-content').stop(true,true).hide()
-													 .first().show();
+        $this.next().children('.tab-content').stop(true,true).hide()
+                           .first().show();
 
-				$this.children('li').first().addClass('active').stop(true,true).show();
-			});
+        $this.children('li').first().addClass('active').stop(true,true).show();
+      });
 
-			$tabsNavLis.on('click', function(e) {
-				var $this = $(this);
+      $tabsNavLis.on('click', function(e) {
+        var $this = $(this);
 
-				$this.siblings().removeClass('active').end()
-					 .addClass('active');
+        $this.siblings().removeClass('active').end()
+           .addClass('active');
 
-				$this.parent().next().children('.tab-content').stop(true,true).hide()
-															  .siblings( $this.find('a').attr('href') ).fadeIn();
+        $this.parent().next().children('.tab-content').stop(true,true).hide()
+                                .siblings( $this.find('a').attr('href') ).fadeIn();
 
-				e.preventDefault();
-			});
+        e.preventDefault();
+      });
 
-		})();
-		
-			
+    })();
+    
+      
 });
 
 /* ------------------ Tooltips ----------------- */
@@ -101,18 +101,18 @@ jQuery(document).ready(function() {
 
 });
 
-/* ------------------ Progress Bar ------------------- */	
+/* ------------------ Progress Bar ------------------- */  
 
 jQuery(document).ready(function($){
-	
-	$(".meter > span").each(function() {
-		$(this)
-		.data("origWidth", $(this).width())
-		.width(0)
-		.animate({
-			width: $(this).data("origWidth")
-		}, 1200);
-	});
+  
+  $(".meter > span").each(function() {
+    $(this)
+    .data("origWidth", $(this).width())
+    .width(0)
+    .animate({
+      width: $(this).data("origWidth")
+    }, 1200);
+  });
 });
 
 /* ------------------- Parallax --------------------- */
@@ -129,89 +129,89 @@ jQuery(document).ready(function($){
 /* ------------------ Image Overlay ----------------- */
 
 jQuery(document).ready(function () {
-	
-	$('.picture a').hover(function () {
-		$(this).find('.image-overlay-zoom, .image-overlay-link, .image-overlay-play').stop().fadeTo('fast', 1);
-	},function () {
-		$(this).find('.image-overlay-zoom, .image-overlay-link, .image-overlay-play').stop().fadeTo('fast', 0);
-	});
-	
+  
+  $('.picture a').hover(function () {
+    $(this).find('.image-overlay-zoom, .image-overlay-link, .image-overlay-play').stop().fadeTo('fast', 1);
+  },function () {
+    $(this).find('.image-overlay-zoom, .image-overlay-link, .image-overlay-play').stop().fadeTo('fast', 0);
+  });
+  
 });
 
 /* -------------------- Isotope --------------------- */
 
 jQuery(document).ready(function (){
-	
-	$('#portfolio-wrapper').imagesLoaded(function(){
-		
-		var $container = $('#portfolio-wrapper');
-			$select = $('#filters select');
+  
+  $('#portfolio-wrapper').imagesLoaded(function(){
+    
+    var $container = $('#portfolio-wrapper');
+      $select = $('#filters select');
 
-		// initialize Isotope
-		$container.isotope({
-		// options...
-		resizable: false, // disable normal resizing
-		// set columnWidth to a percentage of container width
-	  	masonry: { columnWidth: $container.width() / 12 }
-		});
+    // initialize Isotope
+    $container.isotope({
+    // options...
+    resizable: false, // disable normal resizing
+    // set columnWidth to a percentage of container width
+      masonry: { columnWidth: $container.width() / 12 }
+    });
 
-		// update columnWidth on window resize
-		$(window).smartresize(function(){
-		
-			$container.isotope({
-			// update columnWidth to a percentage of container width
-			masonry: { columnWidth: $container.width() / 12 }
-			});
-		});
+    // update columnWidth on window resize
+    $(window).smartresize(function(){
+    
+      $container.isotope({
+      // update columnWidth to a percentage of container width
+      masonry: { columnWidth: $container.width() / 12 }
+      });
+    });
 
 
-		$container.isotope({
-			itemSelector : '.portfolio-item'
-		});
+    $container.isotope({
+      itemSelector : '.portfolio-item'
+    });
 
-		$select.change(function() {
-			
-			var filters = $(this).val();
+    $select.change(function() {
+      
+      var filters = $(this).val();
 
-				$container.isotope({
-					filter: filters
-				});
-			
-			});
+        $container.isotope({
+          filter: filters
+        });
+      
+      });
 
-			var $optionSets = $('#filters .option-set'),
-		  	$optionLinks = $optionSets.find('a');
+      var $optionSets = $('#filters .option-set'),
+        $optionLinks = $optionSets.find('a');
 
-		  	$optionLinks.click(function(){
-			
-				var $this = $(this);
-				// don't proceed if already selected
-				if ( $this.hasClass('selected') ) {
-			  		return false;
-				}
-			var $optionSet = $this.parents('.option-set');
-			$optionSet.find('.selected').removeClass('selected');
-			$this.addClass('selected');
+        $optionLinks.click(function(){
+      
+        var $this = $(this);
+        // don't proceed if already selected
+        if ( $this.hasClass('selected') ) {
+            return false;
+        }
+      var $optionSet = $this.parents('.option-set');
+      $optionSet.find('.selected').removeClass('selected');
+      $this.addClass('selected');
 
-			// make option object dynamically, i.e. { filter: '.my-filter-class' }
-			var options = {},
-				key = $optionSet.attr('data-option-key'),
-				value = $this.attr('data-option-value');
-			// parse 'false' as false boolean
-			value = value === 'false' ? false : value;
-			options[ key ] = value;
-			if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-			  // changes in layout modes need extra logic
-			  changeLayoutMode( $this, options );
-			} else {
-			  // otherwise, apply new options
-			  $container.isotope( options );
-			}
+      // make option object dynamically, i.e. { filter: '.my-filter-class' }
+      var options = {},
+        key = $optionSet.attr('data-option-key'),
+        value = $this.attr('data-option-value');
+      // parse 'false' as false boolean
+      value = value === 'false' ? false : value;
+      options[ key ] = value;
+      if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
+        // changes in layout modes need extra logic
+        changeLayoutMode( $this, options );
+      } else {
+        // otherwise, apply new options
+        $container.isotope( options );
+      }
 
-			return false;
-			
-		  });
-		
-	});
-	
+      return false;
+      
+      });
+    
+  });
+  
 });
